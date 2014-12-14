8e31a03da3d486fae86aa82d7e474ce3a9cd937b
Jules Bonnard <julesbonnard@gmail.com>
2014-12-14 16:42:52 +0100

    Remove teaser

--- js/intro.js
+++ js/intro.js
@@ -3,7 +3,8 @@ $(function() {
     $("#industries").hover(
         function() {
             $(this).attr("src", "img/choix_industries_hover.png");
-        }, function() {
+        },
+        function() {
             $(this).attr("src", "img/choix_industries.png");
         }
     );
@@ -11,7 +12,8 @@ $(function() {
     $("#chauffage").hover(
         function() {
             $(this).attr("src", "img/choix_chauffage_hover.png");
-        }, function() {
+        },
+        function() {
             $(this).attr("src", "img/choix_chauffage.png");
         }
     );
@@ -19,7 +21,8 @@ $(function() {
     $("#trafic").hover(
         function() {
             $(this).attr("src", "img/choix_trafic_hover.png");
-        }, function() {
+        },
+        function() {
             $(this).attr("src", "img/choix_trafic.png");
         }
     );
@@ -35,20 +38,10 @@ $(function() {
 
         froogaloop.addEvent('pause',
             function(data) {
-                var current_time,
-                    duration;
-                froogaloop.api('getCurrentTime', function(value, player_id) {
-                    current_time = value;
-                    froogaloop.api('getDuration', function(value, player_id) {
-                        duration = value;
-                        if (current_time < duration-2) {
-                            var lavideo = $("#iframe-video").attr("data-vid");
-                            if (lavideo == "A") {
-                                $("#skip_teaser").fadeIn(300);
-                            } else {
-                                $("#skip_intro").fadeIn(300);
-                            }
-
+                froogaloop.api('getCurrentTime', function(current_time, player_id) {
+                    froogaloop.api('getDuration', function(duration, player_id) {
+                        if (current_time < duration - 2) {
+                            $('#choix').fadeIn(300);
                         }
                     });
                 });
@@ -57,7 +50,6 @@ $(function() {
 
         froogaloop.addEvent('play',
             function(data) {
-                $(".skip").fadeOut();
                 $('#choix').fadeOut(300);
             });
 
@@ -66,54 +58,26 @@ $(function() {
         froogaloop.addEvent('finish',
             function(data) {
                 var lavideo = $("#iframe-video").attr("data-vid");
-                if (lavideo == "A") {
-                    $("#iframe-video").attr("src", "http://player.vimeo.com/video/92084718?api=1&amp;player_id=iframe-video&amp;color=d7c49b&amp;autoplay=1");
-                    $("#iframe-video").attr("data-vid", "B");
-                } else {
-                    //$('#vimeo').html('<img id="img_fond" class="all_screen" src="img/img_fond.png" width="1000" height="580" />');
-                    // $(".outro-wrapper").fadeOut();
-                    // $("#choix").fadeIn(300);
-                }
+                $('#vimeo').html('<img id="img_fond" class="all_screen" src="img/img_fond.png" width="1000" height="580" />');
+                $(".outro-wrapper").fadeOut();
+                $("#choix").fadeIn(300);
             });
 
         froogaloop.addEvent('playProgress',
             function(data) {
-                var lavideo = $("#iframe-video").attr("data-vid");
-                if (lavideo == "B") {
-                    var current_time,
-                        duration;
-                    froogaloop.api('getCurrentTime', function(value, player_id) {
-                        current_time = value;
-                        froogaloop.api('getDuration', function(value, player_id) {
-                            duration = value;
-                            if (current_time > duration-7) {
-                                $("#choix").fadeIn(300);
-                            }
-                            if (current_time < duration-7) {
-                                $("#choix").fadeOut(300);
-                            }
-                            if (current_time > duration-0.2) {
-                                froogaloop.api('pause');
-                            }
-                        });
+                froogaloop.api('getCurrentTime', function(current_time, player_id) {
+                    froogaloop.api('getDuration', function(duration, player_id) {
+                        if (current_time > duration - 7) {
+                            $("#choix").fadeIn(300);
+                        }
+                        if (current_time < duration - 7) {
+                            $("#choix").fadeOut(300);
+                        }
+                        if (current_time > duration - 0.5) {
+                            froogaloop.api('pause');
+                        }
                     });
-                }
+                });
             });
     }
-
-    $("#skip_teaser").on('click',function() {
-        $('.skip').fadeOut();
-        $("#iframe-video").attr("src", "http://player.vimeo.com/video/92084718?api=1&amp;player_id=iframe-video&amp;color=d7c49b&amp;autoplay=1");
-        $("#iframe-video").attr("data-vid", "B");
-        $('#iframe-video').load(function(e) {
-            froogaloop.api('play');
-        });
-    });
-    $("#skip_intro").on('click',function() {
-        $('.skip').fadeOut();
-        $('#iframe-video').load(function(e) {
-            froogaloop.api('pause');
-        });
-        $('#choix').fadeIn(300);
-    });
 });
